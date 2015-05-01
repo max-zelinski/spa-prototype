14193503ee962f2b65a0ab4bbc3a6e04a55a7c3d
max-zelinski <max.zelinski@gmail.com>
2015-05-01 18:23:20 +0300

    tabs to spaces

--- react-reflux-router/js/stores/paymentsStore.js
+++ react-reflux-router/js/stores/paymentsStore.js
@@ -3,34 +3,34 @@ var Reflux = require('reflux'),
     _ = require('lodash');
 
 var Api = require('../api/paymentsApi'),
-		AccountsStore = require('./accountsStore');
+    AccountsStore = require('./accountsStore');
 
 module.exports = Reflux.createStore({
-	_latestPayments: [],
-	init: function() {
-		this.listenTo(AccountsStore, this.onAccountsUpdated);
-	},
-	getLatestPayments: function() {
+  _latestPayments: [],
+  init: function() {
+    this.listenTo(AccountsStore, this.onAccountsUpdated);
+  },
+  getLatestPayments: function() {
     if (this._latestPayments.length !== 0) {
       return this._latestPayments;
     }
 
     var that = this;
-		var deffered = Q.defer();
-		AccountsStore.getAllAccounts().then(function(accounts) {
-			return Q.all(accounts.map(function(account) {
-				return Api.getPayments(account.id);
-			}));
-		}).then(function(payments) {
+    var deffered = Q.defer();
+    AccountsStore.getAllAccounts().then(function(accounts) {
+      return Q.all(accounts.map(function(account) {
+        return Api.getPayments(account.id);
+      }));
+    }).then(function(payments) {
       that._latestPayments = _.flatten(payments);
       that.trigger();
-		});
+    });
 
-		return [];
-	},
-	onAccountsUpdated: function() {
-		this._latestPayments = [];
+    return [];
+  },
+  onAccountsUpdated: function() {
+    this._latestPayments = [];
     this.getLatestPayments();
     this.trigger();
-	}
+  }
 });
--- react-reflux-router/js/widgets/paymentsWidget.jsx
+++ react-reflux-router/js/widgets/paymentsWidget.jsx
@@ -1,39 +1,39 @@
 var React = require('react'),
-		Reflux = require('reflux');
+    Reflux = require('reflux');
 
 var Store = require('../stores/paymentsStore');
 
 module.exports = React.createClass({
-	mixins: [Reflux.listenTo(Store, 'onPaymentsUpdated')],
-	getInitialState: function() {
-		return {
-			payments: Store.getLatestPayments()
-		};
-	},
-	onPaymentsUpdated: function(state) {
-		this.setState({
-			payments: Store.getLatestPayments()
-		});
-	},
-	render: function() {
-		var payments;
-		if (this.state.payments.length === 0) {
-			payments = <div>Loading</div>;
-		}
-		else {
-			payments = this.state.payments.map(function(payment) {
-				return (<li key={payment.id}>{payment.name}</li>);
-			});
-		}
+  mixins: [Reflux.listenTo(Store, 'onPaymentsUpdated')],
+  getInitialState: function() {
+    return {
+      payments: Store.getLatestPayments()
+    };
+  },
+  onPaymentsUpdated: function(state) {
+    this.setState({
+      payments: Store.getLatestPayments()
+    });
+  },
+  render: function() {
+    var payments;
+    if (this.state.payments.length === 0) {
+      payments = <div>Loading</div>;
+    }
+    else {
+      payments = this.state.payments.map(function(payment) {
+        return (<li key={payment.id}>{payment.name}</li>);
+      });
+    }
 
-		return (
-			<div>
-				<h2>Payments Widget</h2>
-				<p>Latest payments:</p>
-				<ul>
-					{payments}
-				</ul>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <h2>Payments Widget</h2>
+        <p>Latest payments:</p>
+        <ul>
+          {payments}
+        </ul>
+      </div>
+    );
+  }
 });
