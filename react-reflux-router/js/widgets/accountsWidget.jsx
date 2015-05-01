ac32f954ea415353b05b70ee2c24ac48dada2826
max-zelinski <max.zelinski@gmail.com>
2015-05-01 22:18:22 +0300

    added select current account functionality

--- react-reflux-router/js/widgets/accountsWidget.jsx
+++ react-reflux-router/js/widgets/accountsWidget.jsx
@@ -3,7 +3,8 @@ var React = require('react'),
     ReactAsync = require('react-async'),
     Q = require('q');
 
-var Store = require('../stores/accountsStore');
+var Store = require('../stores/accountsStore'),
+    Actions = require('../actions/accountsActions');
 
 var AccountsWidget = React.createClass({
   mixins: [
@@ -27,19 +28,46 @@ var AccountsWidget = React.createClass({
       that.setState(state);
     });
 	},
+  changeCurrentAccount: function(e) {
+    e.preventDefault();
+
+    var accountId = e.target.value;
+    if (accountId == 'empty') {
+      return;
+    }
+
+    this.state.accounts.some(function(account) {
+      if (account.id == accountId) {
+        Actions.changeCurrentAccount(account);
+        return true;
+      }
+      return false;
+    });
+  },
   render: function() {
+    var currentAccount = this.state.currentAccount;
+    var currentAccountId = currentAccount !== null ? currentAccount.id : 'empty';
     return (
       <div>
         <h2>Accounts Widget</h2>
+
         <p>Accounts:</p>
         <ul>
           {this.state.accounts.map(function(account) {
             return <li key={account.id}>{account.name}</li>;
           })}
         </ul>
-    </div>
-  );
-}
+
+        <p>Current account:</p>
+        <select onChange={this.changeCurrentAccount} value={currentAccountId}>
+          <option value='empty'/>
+          {this.state.accounts.map(function(account){
+            return <option key={account.id} value={account.id}>{account.name}</option>;
+          })}
+        </select>
+      </div>
+    );
+  }
 });
 
 module.exports = React.createClass({
