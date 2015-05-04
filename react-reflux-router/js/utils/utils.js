868280ce423a0c4d27f0eb39f089b2c1174b4ad4
max-zelinski <max.zelinski@gmail.com>
2015-05-04 14:28:22 +0300

    cache was not clearing properly

--- react-reflux-router/js/utils/utils.js
+++ react-reflux-router/js/utils/utils.js
@@ -3,8 +3,9 @@ var memoize = require('memoize');
 module.exports.throttlePromise = function(operation) {
   var memoizedfun = memoize(operation);
   return function() {
-    return memoizedfun.apply(this, arguments).finally(function() {
-      memoizedfun.__cache.remove();
+    var args = arguments;
+    return memoizedfun.apply(this, args).finally(function() {
+      memoizedfun.__cache.remove.apply(null, args);
     });
   };
 };
